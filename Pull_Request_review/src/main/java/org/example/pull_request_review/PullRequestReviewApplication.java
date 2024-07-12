ef167d2abd9f343a20accad7bf91325cf089b035
Sajith Sajan Koshy <126872654+sajithkoshy@users.noreply.github.com>
2024-07-12 12:21:04 +0530

    initial commit

---
+++ Pull_Request_review/src/main/java/org/example/pull_request_review/PullRequestReviewApplication.java
@@ -0,0 +1,13 @@
+package org.example.pull_request_review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PullRequestReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PullRequestReviewApplication.class, args);
+    }
+
+}
